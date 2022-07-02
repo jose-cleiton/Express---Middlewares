e4c92700676ac87040a0d43ecf2539cbfb635289
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-07-02 17:34:26 -0300

    Tirando o authMiddleware do index e pondo nas rotas

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { authMiddleware } = require('./middleware');
+
 
 const app = express();
 app.use(bodyParser.json());
-app.use(authMiddleware);
 
-const recipesRouter = require('./router/recipesRouter');
-app.use('/recipes', recipesRouter);
+app.use('/recipes',  require('./router/recipesRouter'));
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
