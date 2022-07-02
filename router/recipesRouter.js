626d48f6b9539ddba5415749db89ba1402173658
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-07-02 17:34:02 -0300

    Colocando o authMiddleware na rotas

--- router/recipesRouter.js
+++ router/recipesRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../service/Author');
+const { authMiddleware } = require('../middleware');
 
-
+router.use(authMiddleware)
 router.get('/search',  Author.getSearchByName_maxPrice);
 router.get('/',  Author.get);
-
 router.get('/:id', Author.getById);
 
 router.post('/', Author.post);
